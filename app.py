eaa52bb4b8387f29022e41fb3ee9fdb80b78b002
Sudarshan Kumar Surubhatla <143895204+sudarshan-surubhatla@users.noreply.github.com>
2024-11-27 21:31:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,5 +135,5 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
